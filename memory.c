f72839ee6e8c34c9b0d932cda6968c613752f5e6
Dafinci01 <odelanadavid@gmail.com>
2023-12-13 21:10:47 -0800

    updated memory .c

--- memory.c
+++ memory.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * bfree -  a pointer with null it
+ * bfree - frees a pointer and NULLs the address
  * @ptr: address of the pointer to free
  *
  * Return: 1 if freed, otherwise 0.
